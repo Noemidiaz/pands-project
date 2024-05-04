646f5f97de659c94cb4299a848e251c16760b097
Noemi Diaz <noemi.diazglez@gmail.com>
2024-05-04 20:06:42 +0100

    importing data frame

--- analysis.py
+++ analysis.py
@@ -1,3 +1,23 @@
 # analysis.py
-#
+# 
 # Author: Noemi Diaz
+
+
+
+# Imports and libraries
+
+import pandas as pd
+
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+
+# Load the data 
+
+df = pd.read_csv ("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
+
+
+# Have a look the data
+df
+

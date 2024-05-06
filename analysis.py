0644f8f6a997be48868569245bf411904eae4cea
Noemi Diaz <noemi.diazglez@gmail.com>
2024-05-06 21:12:27 +0100

    Examining the data set

--- analysis.py
+++ analysis.py
@@ -3,13 +3,11 @@
 # Author: Noemi Diaz
 
 
-
 # Imports and libraries
 
 import pandas as pd
-
 import seaborn as sns
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -18,6 +16,12 @@ import matplotlib.pyplot as plt
 df = pd.read_csv ("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
 
 
+# Examining data set
 # Have a look the data
-df
+print(df) 
 
+# Look at the first row
+print (df.iloc [0])
+
+# Analysis of the species
+print (df ["species"])
